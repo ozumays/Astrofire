258cc3ede5be920c0def72e7fd1058d52ceecbd1
ozumays <astrozumays@gmail.com>
2026-03-04 01:25:14 +0300

    Add files via upload

---
+++ fixed_stars_data.py
@@ -0,0 +1,242 @@
+# fixed_stars_data.py
+# Sabit Yıldızların Takımyıldızlarına Göre Gruplandırılmış Geniş Veri Yapısı
+
+FIXED_STAR_DATA_HIERARCHY = {
+    "ANDROMEDA": [
+        {"name": "Alpheratz", "swe_name": "Alpheratz"},
+        {"name": "Mirach", "swe_name": "Mirach"},
+        {"name": "Almach", "swe_name": "Almach"},
+        {"name": "Adhil", "swe_name": "Adhil"},
+    ],
+    "AQUARIUS": [
+        {"name": "Sadalmelik", "swe_name": "Sadalmelik"},
+        {"name": "Sadalsuud", "swe_name": "Sadalsuud"},
+        {"name": "Skat", "swe_name": "Skat"},
+        {"name": "Albali", "swe_name": "Albali"},
+        {"name": "Ancha", "swe_name": "Ancha"},
+    ],
+    "AQUILA": [
+        {"name": "Altair", "swe_name": "Altair"},
+        {"name": "Alshain", "swe_name": "Alshain"},
+        {"name": "Tarazed", "swe_name": "Tarazed"},
+    ],
+    "ARIES": [
+        {"name": "Hamal", "swe_name": "Hamal"},
+        {"name": "Sheratan", "swe_name": "Sheratan"},
+        {"name": "Mesarthim", "swe_name": "Mesarthim"},
+        {"name": "Botein", "swe_name": "Botein"},
+    ],
+    "AURIGA": [
+        {"name": "Capella", "swe_name": "Capella"},
+        {"name": "Menkalinan", "swe_name": "Menkalinan"},
+        {"name": "Elnath", "swe_name": "Elnath"}, # Boğa ile ortak
+    ],
+    "BOOTES": [
+        {"name": "Arcturus", "swe_name": "Arcturus"},
+        {"name": "Nekkar", "swe_name": "Nekkar"},
+        {"name": "Izar", "swe_name": "Izar"},
+        {"name": "Muphrid", "swe_name": "Muphrid"},
+        {"name": "Seginus", "swe_name": "Seginus"},
+        {"name": "Princeps", "swe_name": "Princeps"},
+    ],
+    "CANCER": [
+        {"name": "Acubens", "swe_name": "Acubens"},
+        {"name": "Asellus Borealis", "swe_name": "Asellus Borealis"},
+        {"name": "Asellus Australis", "swe_name": "Asellus Australis"},
+        {"name": "Praesepe", "swe_name": "Praesepe"}, # Küme
+    ],
+    "CANIS MAJOR": [
+        {"name": "Sirius", "swe_name": "Sirius"},
+        {"name": "Mirzam", "swe_name": "Mirzam"},
+        {"name": "Wezen", "swe_name": "Wezen"},
+        {"name": "Adhara", "swe_name": "Adhara"},
+        {"name": "Muliphein", "swe_name": "Muliphein"},
+        {"name": "Aludra", "swe_name": "Aludra"},
+    ],
+    "CANIS MINOR": [
+        {"name": "Procyon", "swe_name": "Procyon"},
+        {"name": "Gomeisa", "swe_name": "Gomeisa"},
+    ],
+    "CAPRICORNUS": [
+        {"name": "Deneb Algedi", "swe_name": "Deneb Algedi"},
+        {"name": "Dabih", "swe_name": "Dabih"},
+        {"name": "Nashira", "swe_name": "Nashira"},
+        {"name": "Algedi", "swe_name": "Algedi"},
+    ],
+    "CASSIOPEIA": [
+        {"name": "Schedar", "swe_name": "Schedar"},
+        {"name": "Caph", "swe_name": "Caph"},
+        {"name": "Ruchbah", "swe_name": "Ruchbah"},
+    ],
+    "CENTAURUS": [
+        {"name": "Rigil Kentaurus", "swe_name": "Rigil Kentaurus"},
+        {"name": "Hadar", "swe_name": "Hadar"},
+        {"name": "Menkent", "swe_name": "Menkent"},
+    ],
+    "CETUS": [
+        {"name": "Menkar", "swe_name": "Menkar"},
+        {"name": "Diphda", "swe_name": "Diphda"},
+        {"name": "Mira", "swe_name": "Mira"},
+        {"name": "Baten Kaitos", "swe_name": "Baten Kaitos"},
+    ],
+    "CORONA BOREALIS": [
+        {"name": "Alphecca", "swe_name": "Alphecca"},
+        {"name": "Nusakan", "swe_name": "Nusakan"},
+    ],
+    "CORVUS": [
+        {"name": "Algorab", "swe_name": "Algorab"},
+        {"name": "Gienah", "swe_name": "Gienah"},
+    ],
+    "CRUX": [
+        {"name": "Acrux", "swe_name": "Acrux"},
+        {"name": "Mimosa", "swe_name": "Mimosa"},
+        {"name": "Gacrux", "swe_name": "Gacrux"},
+    ],
+    "CYGNUS": [
+        {"name": "Deneb", "swe_name": "Deneb"},
+        {"name": "Albireo", "swe_name": "Albireo"},
+        {"name": "Sadr", "swe_name": "Sadr"},
+        {"name": "Gienah Cygni", "swe_name": "Gienah Cygni"},
+    ],
+    "DRACO": [
+        {"name": "Thuban", "swe_name": "Thuban"},
+        {"name": "Rastaban", "swe_name": "Rastaban"},
+        {"name": "Eltanin", "swe_name": "Eltanin"},
+        {"name": "Nodus I", "swe_name": "Nodus I"},
+    ],
+    "ERIDANUS": [
+        {"name": "Achernar", "swe_name": "Achernar"},
+        {"name": "Cursa", "swe_name": "Cursa"},
+        {"name": "Zaurak", "swe_name": "Zaurak"},
+        {"name": "Acamar", "swe_name": "Acamar"},
+    ],
+    "GEMINI": [
+        {"name": "Castor", "swe_name": "Castor"},
+        {"name": "Pollux", "swe_name": "Pollux"},
+        {"name": "Alhena", "swe_name": "Alhena"},
+        {"name": "Wasat", "swe_name": "Wasat"},
+        {"name": "Mebsuta", "swe_name": "Mebsuta"},
+        {"name": "Tejat Posterior", "swe_name": "Tejat Posterior"},
+        {"name": "Tejat Prior", "swe_name": "Tejat Prior"},
+        {"name": "Propus", "swe_name": "Propus"},
+    ],
+    "HERCULES": [
+        {"name": "Ras Algethi", "swe_name": "Ras Algethi"},
+        {"name": "Kornephoros", "swe_name": "Kornephoros"},
+    ],
+    "HYDRA": [
+        {"name": "Alphard", "swe_name": "Alphard"},
+    ],
+    "LEO": [
+        {"name": "Regulus", "swe_name": "Regulus"},
+        {"name": "Denebola", "swe_name": "Denebola"},
+        {"name": "Algieba", "swe_name": "Algieba"},
+        {"name": "Zosma", "swe_name": "Zosma"},
+        {"name": "Adhafera", "swe_name": "Adhafera"},
+        {"name": "Ras Elased Australis", "swe_name": "Ras Elased Australis"},
+        {"name": "Ras Elased Borealis", "swe_name": "Ras Elased Borealis"},
+    ],
+    "LIBRA": [
+        {"name": "Zuben Elgenubi", "swe_name": "Zuben Elgenubi"},
+        {"name": "Zuben Eschamali", "swe_name": "Zuben Eschamali"},
+        {"name": "Zuben Elakrab", "swe_name": "Zuben Elakrab"},
+    ],
+    "LYRA": [
+        {"name": "Vega", "swe_name": "Vega"},
+        {"name": "Sheliak", "swe_name": "Sheliak"},
+        {"name": "Sulaphat", "swe_name": "Sulaphat"},
+    ],
+    "OPHIUCHUS": [
+        {"name": "Rasalhague", "swe_name": "Rasalhague"},
+        {"name": "Sabik", "swe_name": "Sabik"},
+        {"name": "Han", "swe_name": "Han"},
+        {"name": "Yed Prior", "swe_name": "Yed Prior"},
+    ],
+    "ORION": [
+        {"name": "Betelgeuse", "swe_name": "Betelgeuse"},
+        {"name": "Rigel", "swe_name": "Rigel"},
+        {"name": "Bellatrix", "swe_name": "Bellatrix"},
+        {"name": "Mintaka", "swe_name": "Mintaka"},
+        {"name": "Alnilam", "swe_name": "Alnilam"},
+        {"name": "Alnitak", "swe_name": "Alnitak"},
+        {"name": "Saiph", "swe_name": "Saiph"},
+        {"name": "Meissa", "swe_name": "Meissa"},
+    ],
+    "PEGASUS": [
+        {"name": "Markab", "swe_name": "Markab"},
+        {"name": "Scheat", "swe_name": "Scheat"},
+        {"name": "Algenib", "swe_name": "Algenib"},
+        {"name": "Enif", "swe_name": "Enif"},
+    ],
+    "PERSEUS": [
+        {"name": "Algol", "swe_name": "Algol"},
+        {"name": "Mirfak", "swe_name": "Mirfak"},
+    ],
+    "PISCES": [
+        {"name": "Alrescha", "swe_name": "Alrescha"},
+        {"name": "Fum Al Samakah", "swe_name": "Fum Al Samakah"},
+    ],
+    "PISCIS AUSTRINUS": [
+        {"name": "Fomalhaut", "swe_name": "Fomalhaut"},
+    ],
+    "SAGITTARIUS": [
+        {"name": "Nunki", "swe_name": "Nunki"},
+        {"name": "Kaus Australis", "swe_name": "Kaus Australis"},
+        {"name": "Kaus Media", "swe_name": "Kaus Media"},
+        {"name": "Kaus Borealis", "swe_name": "Kaus Borealis"},
+        {"name": "Ascella", "swe_name": "Ascella"},
+        {"name": "Arkab", "swe_name": "Arkab"},
+        {"name": "Alnasl", "swe_name": "Alnasl"},
+        {"name": "Rukbat", "swe_name": "Rukbat"},
+    ],
+    "SCORPIUS": [
+        {"name": "Antares", "swe_name": "Antares"},
+        {"name": "Graffias", "swe_name": "Graffias"},
+        {"name": "Dschubba", "swe_name": "Dschubba"},
+        {"name": "Sargas", "swe_name": "Sargas"},
+        {"name": "Shaula", "swe_name": "Shaula"},
+        {"name": "Lesath", "swe_name": "Lesath"},
+        {"name": "Alniyat", "swe_name": "Alniyat"},
+    ],
+    "TAURUS": [
+        {"name": "Aldebaran", "swe_name": "Aldebaran"},
+        {"name": "Elnath", "swe_name": "Elnath"},
+        {"name": "Alcyone", "swe_name": "Alcyone"},
+        {"name": "Pleione", "swe_name": "Pleione"},
+        {"name": "Atlas", "swe_name": "Atlas"},
+        {"name": "Maia", "swe_name": "Maia"},
+        {"name": "Merope", "swe_name": "Merope"},
+        {"name": "Taygeta", "swe_name": "Taygeta"},
+        {"name": "Celaeno", "swe_name": "Celaeno"},
+        {"name": "Electra", "swe_name": "Electra"},
+        {"name": "Hyadum I", "swe_name": "Hyadum I"},
+    ],
+    "URSA MAJOR": [
+        {"name": "Dubhe", "swe_name": "Dubhe"},
+        {"name": "Merak", "swe_name": "Merak"},
+        {"name": "Phecda", "swe_name": "Phecda"},
+        {"name": "Megrez", "swe_name": "Megrez"},
+        {"name": "Alioth", "swe_name": "Alioth"},
+        {"name": "Mizar", "swe_name": "Mizar"},
+        {"name": "Alkaid", "swe_name": "Alkaid"},
+        {"name": "Talitha", "swe_name": "Talitha"},
+        {"name": "Tania Borealis", "swe_name": "Tania Borealis"},
+        {"name": "Tania Australis", "swe_name": "Tania Australis"},
+    ],
+    "URSA MINOR": [
+        {"name": "Polaris", "swe_name": "Polaris"},
+        {"name": "Kochab", "swe_name": "Kochab"},
+    ],
+    "VIRGO": [
+        {"name": "Spica", "swe_name": "Spica"},
+        {"name": "Vindemiatrix", "swe_name": "Vindemiatrix"},
+        {"name": "Porrima", "swe_name": "Porrima"},
+        {"name": "Auva", "swe_name": "Auva"},
+        {"name": "Heze", "swe_name": "Heze"},
+        {"name": "Syrma", "swe_name": "Syrma"},
+        {"name": "Zaniah", "swe_name": "Zaniah"},
+    ]
+}
+
+# astro_core.py için düzleştirilmiş ve tekil liste
+FIXED_STARS_CALC_LIST = list(set([star["swe_name"] for const in FIXED_STAR_DATA_HIERARCHY.values() for star in const]))
